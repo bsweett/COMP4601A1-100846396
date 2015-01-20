19ffb17cbd08fff3a7bcfd350c328d1153cb445d
BraydenGirard <braydengirard@icloud.com>
2015-01-19 20:21:10 -0500

    Updated package for DocumentAction

--- src/edu/carleton/COMP4601/a1/Main/DocumentAction.java
+++ src/edu/carleton/COMP4601/a1/Main/DocumentAction.java
@@ -1,10 +1,15 @@
 package edu.carleton.COMP4601.a1.Main;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.JAXBElement;
 
 import edu.carleton.COMP4601.a1.Model.Action;
 import edu.carleton.COMP4601.a1.Model.Document;
@@ -13,16 +18,52 @@ public class DocumentAction extends Action {
 
 	public DocumentAction(UriInfo uriInfo, Request request, String id) {
 		super(uriInfo, request, id);
+		// TODO Auto-generated constructor stub
 	}
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public Document getDocumentXML() {		
-		Document d = DatabaseManager.getInstance().findDocument(new Integer(id));
-		if (d == null) {
-			throw new RuntimeException("No such document: " + id);
+	public Document getDocumentXML() {
+		Document a = DatabaseManager.getInstance().findDocument(Integer.parseInt(id));
+		if (a == null) {
+			throw new RuntimeException("No such Document: " + id);
 		}
-		return d;
+		return a;
+	}
+	/*
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public Document getDocumentHTML() {
+		Document a = DatabaseManager.getInstance().findDocument(Integer.parseInt(id));
+		if (a == null) {
+			throw new RuntimeException("No such Document: " + id);
+		}
+		return a;
+	}
+*/
+	@PUT
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response putAccount(JAXBElement<Document> doc) {
+		Document d = doc.getValue();
+		return putAndGetResponse(d);
+	}
+
+	@DELETE
+	public void deleteAccount() {
+		if (DatabaseManager.getInstance().removeDocument(Integer.parseInt(id)) == null)
+			throw new RuntimeException("Document " + id + " not found");
 	}
 	
+	private Response putAndGetResponse(Document document) {
+		Response res;
+			Document oldDocument = DatabaseManager.getInstance().findDocument(Integer.parseInt(id));
+			if(oldDocument != null) {
+				DatabaseManager.getInstance().updateDocument(document, oldDocument);
+				res = Response.ok().build();
+			} else {
+				DatabaseManager.getInstance().addNewDocument(document);
+				res = Response.created(uriInfo.getAbsolutePath()).build();
+			}		
+		return res;
+	}
 }
